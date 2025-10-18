85a3917e7b7cb9894ccfce87c8a91b9031e01ff3
projectmydsrt-bro <projectmydsrt@gmail.com>
2025-10-18 16:46:32 +0700

    Update and rename taildwind.config.cjs to tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        dsrt: {
+          50: '#f5f7ff',
+          100: '#e6eeff',
+          500: '#6b6bff'
+        }
+      }
+    }
+  },
+  plugins: []
+}
