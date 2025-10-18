e87681fb90ec28f3ba84b49b03131d4593125dd8
projectmydsrt-bro <projectmydsrt@gmail.com>
2025-10-18 16:48:47 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-import Home from './pages/Home';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import AdminUpload from './pages/AdminUpload'
+import Navbar from './components/Navbar'
 
 export default function App() {
-  return <Home />;
-}
+  return (
+    <div className="min-h-screen bg-neutral-950 text-white flex flex-col">
+      <Navbar />
+      <div className="flex-1 p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<AdminUpload />} />
+        </Routes>
+      </div>
+    </div>
+  )
+  }
