4de3a9d3c56b819aee24da64a12542759b4541d5
projectmydsrt-bro <projectmydsrt@gmail.com>
2025-10-19 01:43:06 +0700

    Create games.js

---
+++ src/data/games.js
@@ -0,0 +1,6 @@
+export default [
+  { title: 'Battle Arena', genre: 'Action', image: '/assets/battle.jpg' },
+  { title: 'Puzzle Flow', genre: 'Puzzle', image: '/assets/puzzle.jpg' },
+  { title: 'City Racer', genre: 'Racing', image: '/assets/racer.jpg' },
+  { title: 'Zombie Attack', genre: 'Shooter', image: '/assets/zombie.jpg' }
+];
