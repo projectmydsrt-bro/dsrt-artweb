2b3254e684fae2adb0b171cf2c14f50ef9a424d3
projectmydsrt-bro <projectmydsrt@gmail.com>
2025-10-19 02:25:56 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // penting supaya Codespaces / jaringan bisa akses
-    port: 5173,
+    host: true, // biar Codespaces dan jaringan luar bisa akses
+    port: 5173, // port default Vite
   },
 });
-"scripts": {
-  "dev": "vite",
-  "build": "vite build",
-  "preview": "vite preview"
-}
-"vite": "^5.3.1",
-"@vitejs/plugin-react": "^4.0.0"
