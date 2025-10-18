f57c2eb246ac4a1c7beb2a9bb18aa37e1e2f218b
projectmydsrt-bro <projectmydsrt@gmail.com>
2025-10-19 00:46:00 +0700

    Add game list to games.js

---
+++ js/games.js
@@ -0,0 +1,38 @@
+const games = [
+  {
+    title: "Block Jumper",
+    image: "assets/blockjumper.jpg",
+    featured: true,
+    url: "games/block-jumper/detail.html"
+  },
+  {
+    title: "Crazy Motorcycle",
+    image: "assets/motorcycle.jpg",
+    featured: true,
+    url: "#"
+  },
+  {
+    title: "Fragen Shooter",
+    image: "assets/fragen.jpg",
+    featured: false,
+    url: "#"
+  },
+  {
+    title: "Farm Merge Valley",
+    image: "assets/farm.jpg",
+    featured: false,
+    url: "#"
+  },
+  {
+    title: "Thief Puzzle",
+    image: "assets/thief.jpg",
+    featured: false,
+    url: "#"
+  },
+  {
+    title: "Skydom",
+    image: "assets/skydom.jpg",
+    featured: false,
+    url: "#"
+  }
+];
