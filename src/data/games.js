7f0ede25045af95b6ef1f78575b08ba7cb50bfc7
projectmydsrt-bro <projectmydsrt@gmail.com>
2025-10-19 02:45:23 +0700

    Create games.js

---
+++ src/data/games.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    title: 'Battle Arena',
+    genre: 'Action',
+    image: 'https://picsum.photos/seed/battle/800/450',
+    rating: '4.5'
+  },
+  {
+    title: 'Puzzle Flow',
+    genre: 'Puzzle',
+    image: 'https://picsum.photos/seed/puzzle/800/450',
+    rating: '4.3'
+  },
+  {
+    title: 'City Racer',
+    genre: 'Racing',
+    image: 'https://picsum.photos/seed/racer/800/450',
+    rating: '4.4'
+  },
+  {
+    title: 'Zombie Attack',
+    genre: 'Shooter',
+    image: 'https://picsum.photos/seed/zombie/800/450',
+    rating: '4.2'
+  },
+  {
+    title: 'Nebula Runner',
+    genre: 'Arcade',
+    image: 'https://picsum.photos/seed/nebula/800/450',
+    rating: '4.6'
+  },
+  {
+    title: 'Island Rescue',
+    genre: 'Adventure',
+    image: 'https://picsum.photos/seed/island/800/450',
+    rating: '4.1'
+  },
+  {
+    title: 'Bubble Pop',
+    genre: 'Casual',
+    image: 'https://picsum.photos/seed/bubble/800/450',
+    rating: '4.0'
+  },
+  {
+    title: 'Space IO',
+    genre: '.io',
+    image: 'https://picsum.photos/seed/space/800/450',
+    rating: '4.3'
+  }
+];
