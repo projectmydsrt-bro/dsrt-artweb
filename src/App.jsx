9fed8c1014c8fe4506a0e40e7701745d166d4865
projectmydsrt-bro <projectmydsrt@gmail.com>
2025-10-19 09:31:16 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import { useState } from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import GameGrid from './components/GameGrid';
-import Footer from './components/Footer';
-import './styles/theme.css';
-import './styles/variables.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import './index.css';
 
-export default function App() {
-  const [search, setSearch] = useState('');
-
-  return (
-    <div className="min-h-screen flex flex-col bg-background text-white">
-      <Header onSearch={setSearch} />
-      <Hero />
-      <GameGrid search={search} />
-      <Footer />
-    </div>
-  );
-}
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
